2d06829a5ce6b99a1004d0df16773857628bea73
warpdesign <nicolas.ramz@gmail.com>
2019-11-27 22:40:26 +0100

    Initial Commit

---
+++ ecco.js
@@ -0,0 +1,39 @@
+let frame = 0;
+let context = null;
+let img = null;
+
+function createCanvas(width, height) {
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    document.body.appendChild(canvas);
+    return canvas.getContext('2d');
+}
+
+function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = url;
+        img.onload = () => {
+            resolve(img);
+        }
+    });
+}
+
+function draw() {
+    const adjusts = generate_for_offset(frame);
+    for (let y = 0; y < 224; y++) {
+        const x = adjusts[y];
+        context.drawImage(img, 0, y, 384, 1, -x, y, 384, 1);
+    }
+    frame++;
+    requestAnimationFrame(draw);
+}
+
+async function init() {
+    context = createCanvas(320, 224);
+    img = await loadImage('background.png');
+    requestAnimationFrame(draw);
+}
+
+init();
